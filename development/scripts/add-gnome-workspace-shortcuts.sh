3152e5c4cfa64b236da704a4430e43c01b7824d3
Jørgensen, Mats <matsjoer@gmail.com>
2020-03-16 15:18:35 +0100

    Add shortcuts for gnome

---
+++ development/scripts/add-gnome-workspace-shortcuts.sh
@@ -0,0 +1,19 @@
+dconf write /org/gnome/desktop/wm/keybindings/switch-to-workspace-5 "['<Super>5']"
+dconf write /org/gnome/desktop/wm/keybindings/switch-to-workspace-6 "['<Super>6']"
+dconf write /org/gnome/desktop/wm/keybindings/switch-to-workspace-7 "['<Super>7']"
+dconf write /org/gnome/desktop/wm/keybindings/switch-to-workspace-8 "['<Super>8']"
+dconf write /org/gnome/desktop/wm/keybindings/switch-to-workspace-9 "['<Super>9']"
+
+dconf write /org/gnome/desktop/wm/keybindings/move-to-workspace-5 "['<Shift><Super>5']"
+dconf write /org/gnome/desktop/wm/keybindings/move-to-workspace-6 "['<Shift><Super>6']"
+dconf write /org/gnome/desktop/wm/keybindings/move-to-workspace-7 "['<Shift><Super>7']"
+dconf write /org/gnome/desktop/wm/keybindings/move-to-workspace-8 "['<Shift><Super>8']"
+dconf write /org/gnome/desktop/wm/keybindings/move-to-workspace-9 "['<Shift><Super>9']"
+
+dconf write /org/gnome/desktop/wm/keybindings/close "['<Shift><Super>q']"
+dconf write /org/gnome/desktop/wm/keybindings/toggle-fullscreen "['<Super>f']"
+
+dconf write /org/gnome/desktop/wm/keybindings/switch-windows "['<Alt>Tab']"
+dconf write /org/gnome/desktop/wm/keybindings/switch-windows-backward "['<Shift><Alt>Tab', '<Alt>Above_Tab']"
+dconf write /org/gnome/desktop/wm/keybindings/switch-applications "['<super><Alt>Tab']"
+dconf write /org/gnome/desktop/wm/keybindings/switch-applications-backward "['<super><Shift><Alt>Tab', '<super><Alt>Above_Tab']"
